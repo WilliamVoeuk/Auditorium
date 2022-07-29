49c788fc72f1f404c3897695f5b3866c38d1163c
VWilliam <voeuk.william@gmail.com>
2022-07-29 13:27:50 +0200

    First version

--- Assets/cameraRaycast.cs
+++ Assets/cameraRaycast.cs
@@ -17,15 +17,34 @@ public class cameraRaycast : MonoBehaviour
 
         if(hit.collider != null)
         {
-            Debug.Log("touché");
             Debug.DrawRay(ray.origin, ray.direction* 150f, Color.red);
             if(hit.collider.CompareTag("Resize"))
             {
                 Cursor.SetCursor(_mouseResizeTexture, new Vector2(16f, 16f), CursorMode.Auto );
+                if(Input.GetMouseButtonDown(2))
+                {
+                    _activeEffector = hit.collider.transform.parent;
+                }
+                if(_activeEffector != null)
+                {
+                    float radius = Vector2.Distance(_activeEffector.position, hit.point);
+                    _activeEffector.GetComponent<CircleShape>().Radius = radius; 
+
+
+                }
             }
             else if(hit.collider.CompareTag("Move"))
             {
                 Cursor.SetCursor(_mouseMoveTexture, new Vector2(16f, 16f), CursorMode.Auto );
+                if(Input.GetMouseButtonDown(0))
+                {
+                    _activeEffector = hit.collider.transform.parent;
+                }
+                if(_activeEffector != null)
+                {
+                    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    _activeEffector.transform.position = new Vector3(mousePosition.x, mousePosition.y, _activeEffector.transform.position.z);
+                }
             }
             else
             {
@@ -37,5 +56,12 @@ public class cameraRaycast : MonoBehaviour
             Debug.DrawRay(ray.origin, ray.direction* 150f, Color.white);
             Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto );  
         }
+        //Relâche du click
+        if(Input.GetMouseButtonUp(0))
+        {
+            _activeEffector = null;
+        }
     }
+    private Transform _activeEffector;
+
 }

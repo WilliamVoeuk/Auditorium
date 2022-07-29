6007321d6cfdb341b4592095824d102a8b6b1b03
VWilliam <voeuk.william@gmail.com>
2022-07-29 11:42:48 +0200

    Mouse detection/add arrow/Swap Cursor

--- Assets/cameraRaycast.cs
+++ Assets/cameraRaycast.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class cameraRaycast : MonoBehaviour
 {
-    [SerializeField] 
-    private LayerMask _layerMask;
-
+    [SerializeField] private LayerMask _layerMask;
+    [Header("Cursor Texture")]
+    [SerializeField] private Texture2D _mouseMoveTexture;
+    [SerializeField] private Texture2D _mouseResizeTexture;
     // Update is called once per frame
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction* 150f, Color.white);
         
         RaycastHit2D hit = Physics2D.GetRayIntersection(ray, 150f, _layerMask);
 
@@ -19,7 +19,23 @@ public class cameraRaycast : MonoBehaviour
         {
             Debug.Log("touché");
             Debug.DrawRay(ray.origin, ray.direction* 150f, Color.red);
-
+            if(hit.collider.CompareTag("Resize"))
+            {
+                Cursor.SetCursor(_mouseResizeTexture, new Vector2(16f, 16f), CursorMode.Auto );
+            }
+            else if(hit.collider.CompareTag("Move"))
+            {
+                Cursor.SetCursor(_mouseMoveTexture, new Vector2(16f, 16f), CursorMode.Auto );
+            }
+            else
+            {
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto );
+            }
+        }
+        else
+        {
+            Debug.DrawRay(ray.origin, ray.direction* 150f, Color.white);
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto );  
         }
     }
 }
